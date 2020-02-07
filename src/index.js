73936bf5413e602af3bf8cf239ead81a243ea43b
Jack Mazza <jmazza097@gmail.com>
2020-02-07 13:02:56 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/* change*/
 
 class Square extends React.Component {
     render() {
@@ -12,7 +11,7 @@ class Square extends React.Component {
       );
     }
   }
-  
+
   class Board extends React.Component {
     renderSquare(i) {
       return <Square value = {i}/>;
