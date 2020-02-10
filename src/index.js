8857e62e7157f3a7e850147ac54f68e90b39d86b
Jack Mazza <jmazza097@gmail.com>
2020-02-10 13:38:56 -0600

    click function putting x on board

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import './index.css';
 /*laptop comment*/
 
 class Square extends React.Component {
-    render() {
+   constructor(props){
+     super(props);
+     this.state = {
+       value : null,
+     };
+   }
+  
+  render() {
       return (
-        <button className="square" onClick={function() { alert('click'); }}>\
-          {this.props.value}
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
